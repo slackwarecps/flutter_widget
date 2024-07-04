1f8899d971c128a0f1da24c668725856cac9cab0
FABIO ALVARO PEREIRA <fabio.alvaro@portoseguro.com.br>
2024-07-04 05:31:35 -0300

    Adicionada a classificacao de estrelas

--- lib/main.dart
+++ lib/main.dart
@@ -82,14 +82,45 @@ class _TaskState extends State<Task> {
                       ),
                     ),
                     Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        SizedBox(
+                        Container(
                           width: 200,
                           child: Text(
                             widget.nome,
                             style: const TextStyle(
                                 fontSize: 24, overflow: TextOverflow.ellipsis),
                           ),
+                        ),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.star,
+                              size: 15,
+                              color: Colors.blue,
+                            ),
+                            Icon(
+                              Icons.star,
+                              size: 15,
+                              color: Colors.blue,
+                            ),
+                            Icon(
+                              Icons.star,
+                              size: 15,
+                              color: Colors.blue,
+                            ),
+                            Icon(
+                              Icons.star,
+                              size: 15,
+                              color: Colors.blue[100],
+                            ),
+                            Icon(
+                              Icons.star,
+                              size: 15,
+                              color: Colors.blue[100],
+                            ),
+                          ],
                         ),
                       ],
                     ),
