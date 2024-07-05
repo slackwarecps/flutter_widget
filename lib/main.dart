8f0adc2ef3497f5199af6c5b2febf91f227e7f6f
FABIO ALVARO PEREIRA <fabio.alvaro@portoseguro.com.br>
2024-07-05 05:59:05 -0300

    refactor: novo formulario

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_morango_vscode/screens/initial_screen.dart';
+import 'package:flutter_morango_vscode/screens/form_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const InitialScreen(),
+      home: const FormScreen(),
     );
   }
 }
